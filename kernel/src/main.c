3e9ee6282a8fa4ed43b1b068a3a6a474263b8225
Natalia Nao <kuba19789045535@gmail.com>
2025-12-20 19:24:37 +0100

    Made the exception handler dispatch to specific IRQ handlers, and made an irq0 handler that currently just prints 'tick'

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -3,16 +3,20 @@
 #include <drivers/serial.h>
 #include <int/idt.h>
 #include <int/pit.h>
+#include <int/idt.h>
+#include <int/pit.h>
 
 int main() {
     serial_init();
     serial_write("\x1b[2J\x1b[HHello, world!\n");
 
     idt_init();
-    pit_init(1000);
-    // serial_write("IDT initialized triggering int $3 t test\n");
-    // __asm__ volatile ("int $32");
-    // serial_write("returned from interrupt\n");
+    pit_init(100);  //100 Hz = 10ms ticks
     
+    serial_write("Timer started waiting for ticks\n");
+    
+    while (1) {
+        __asm__ volatile ("hlt");
+    }
     return 0;
 }
